07b29154887afb1cb4cd690da4fc668e975b65c2
Ranjan Shrestha <ranjan@sharingapples.com>
2019-05-14 13:14:09 +0545

    Release v0.1.0

---
+++ docs/precache-manifest.ca0f281e67d41d6bca5ac41bcb2e0386.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51640ad19d7e1cecb2e128266ec9dbb3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "40017ad0773e52473f83",
+    "url": "./static/css/main.19632ca4.chunk.css"
+  },
+  {
+    "revision": "96be01a02b42c8fc1272",
+    "url": "./static/js/2.87861abf.chunk.js"
+  },
+  {
+    "revision": "40017ad0773e52473f83",
+    "url": "./static/js/main.a99dc583.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c9dad9427db27744d7f84b2766ce579.js"
+  "./precache-manifest.ca0f281e67d41d6bca5ac41bcb2e0386.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
