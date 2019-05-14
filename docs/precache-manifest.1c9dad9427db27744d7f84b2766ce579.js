258108c42250d8ff55c69d01b269a769f155be87
Ranjan Shrestha <ranjan@sharingapples.com>
2019-05-14 13:02:40 +0545

    Prepare v0.1.0

---
+++ docs/precache-manifest.1c9dad9427db27744d7f84b2766ce579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286b355dcb360db38b4984ea864e79c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40017ad0773e52473f83",
+    "url": "/static/css/main.19632ca4.chunk.css"
+  },
+  {
+    "revision": "96be01a02b42c8fc1272",
+    "url": "/static/js/2.87861abf.chunk.js"
+  },
+  {
+    "revision": "40017ad0773e52473f83",
+    "url": "/static/js/main.a99dc583.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
