0a8d49d5a42e78d363a39e16ab46a43361c8c606
Ranjan Shrestha <ranjan@sharingapples.com>
2020-01-20 09:33:16 +0545

    Add feature to download records as CSV

--- src/screens/Home/Records.js
+++ src/screens/Home/Records.js
@@ -40,6 +40,33 @@ export default function Records({ records, setSelected, selected }) {
     });
   }, [records, filter]);
 
+  const downloadCSV = useCallback(() => {
+    const filename = `formish-records-${new Date().toISOString()}.csv`;
+    const config = storage.loadConfig();
+    const keys = config.structure.map(m => m.name);
+
+    let text = keys.map(k => JSON.stringify(k)).join(',') + "\r\n";
+    filteredRecords.forEach((id) => {
+      const record = storage.getRecord(id);
+      text += keys.map(k => {
+        const v = record[k];
+        if (v === undefined || v === null) {
+          return '';
+        } else {
+          return JSON.stringify(v);
+        }
+      }).join(',') + "\r\n";
+    });
+
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }, [filteredRecords]);
+
   function ItemRow(props) {
     return <Row {...props} setSelected={setSelected} selected={selected} />
   };
@@ -76,6 +103,9 @@ export default function Records({ records, setSelected, selected }) {
         <b>Available Records</b>
         <input value={filter} onChange={updateFilter} placeholder="Search" />
         {`Records ${filteredRecords.length}/${records.length}`}
+        <div style={{ flex: 1, textAlign: 'right'}}>
+          <button onClick={downloadCSV}>Download CSV</button>
+        </div>
       </div>
       {Body}
     </div>
